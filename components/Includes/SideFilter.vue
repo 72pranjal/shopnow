918b2d5e5de1d0b015bf491aada7472dd2fce81c
72pranjal <pranjal.gupta@vidyamantra.com>
2022-12-10 02:09:44 +0530

    intial commit

---
+++ components/Includes/SideFilter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Helo filter data render component</h2>
+        {{ sideFilterListData }}
+    </div>
+ </template>
+    
+<script setup>
+    const props = defineProps({
+        sideFilterListData:{
+        type:Array,
+    }});
+</script>
