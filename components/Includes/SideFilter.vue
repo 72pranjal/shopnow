934b991a84daf01380e6e1f0f3a2b3c6d57e865f
jitendra kumar <38560853+jitendra1996@users.noreply.github.com>
2022-12-10 20:11:16 +0530

    Update side filter functionality (#10)
    * Updated functionality of side filter
    
    * removed top border from filter categories

--- components/Includes/SideFilter.vue
+++ components/Includes/SideFilter.vue
@@ -1,41 +1,75 @@
 <template>
-    <div class="bg-orange-300 p-2">
+    <div class="p-2">
         <div class="border border-gray-300 p-2">
             <h1>Filters</h1>
-            <div>
+            <div v-if="checkedFilters.length !== 0">
                 <h3 class="mb-2">Applied filters</h3>
                 <ul class="mb-2">
-                    <li class="flex">
-                        <XMarkIcon class="w-4 mr-2" />
-                        <span>Category: Kurtas</span>
+                    <li v-for="appliedFilter, index in checkedFilters" :key="index" class="flex">
+                        <XMarkIcon class="w-4 mr-2 cursor-pointer" @click="removeFilters(appliedFilter)" />
+                        <span>{{ appliedFilter }}</span>
                     </li>
                 </ul>
-                <button class="border border-gray-400 px-3 py-1 rounded-md text-gray-600">Clear All</button>
+                <button class="border border-gray-400 px-3 py-1 rounded-md text-gray-600"
+                    @click="removeAllFilters()">Clear All</button>
             </div>
         </div>
-        <div class="border border-gray-300 p-2">
+        <div v-for="filter, index in filters" :key="index" class="border-l border-r border-b border-gray-300 p-2">
             <div class="flex justify-between">
-                <h1>Category</h1>
-                <ChevronUpIcon :class="[isSubFilters ? '-rotate-180' : '', 'duration-300 w-4 cursor-pointer']"
-                    @click="showSubFilters()" />
+                <h1>{{ filter.filter_lable }}</h1>
+                <ChevronUpIcon
+                    :class="[openCat.includes(filter.filter_lable) ? '-rotate-180' : '', 'duration-300 w-4 cursor-pointer']"
+                    @click="showSubFilters(filter.filter_lable)" />
             </div>
-            <ul v-if="isSubFilters">
-                <li class="cursor-pointer">
-                    <input type="checkbox" id="kurta" value="Kurta" v-model="checkedFilters"
-                        class="mr-2 checked:bg-slate-400">
-                    <label for="kurta" class="cursor-pointer">Kurta</label>
+            <ul v-if="openCat.includes(filter.filter_lable)">
+                <li v-for="subFilter, index in filter.options" :key="index" class="cursor-pointer">
+                    <input type="checkbox" :id="subFilter.value" :value="filter.filter_lable + ': ' + subFilter.value"
+                        v-model="checkedFilters" class="mr-2 checked:bg-slate-400">
+                    <label :for="subFilter.value" class="cursor-pointer mr-3">{{ subFilter.value }}</label>
+                    <span class="text-[.9rem] text-gray-600">({{ subFilter.total }})</span>
                 </li>
             </ul>
         </div>
     </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import { XMarkIcon, ChevronUpIcon } from '@heroicons/vue/24/outline';
+import { filters } from '@/assets/jsons/sideFiltersList';
 
-const checkedFilters = ref([]);
-const isSubFilters = ref(false);
+// variable to handle all checked filters
+const checkedFilters = ref<string[]>([]);
 
-const showSubFilters = () => {
-    isSubFilters.value = !isSubFilters.value;
+// variable to store all categories whose subfilters are visible
+const openCat = ref<string[]>([]);
+
+/**
+ * Function to handle visiblity of sub filters 
+ * @param productId : String 
+ */
+const showSubFilters = (productId: string) => {
+    if (!openCat.value.includes(productId)) {
+        openCat.value.push(productId);
+    } else {
+        const index = openCat.value.indexOf(productId);
+        openCat.value.splice(index, 1);
+    }
+}
+
+/**
+ * Funtion to remove particular applied sub filter
+ * @param removedFilterId : string unique_value of applied filters
+ */
+const removeFilters = (removedFilterId: string) => {
+    if (checkedFilters.value.includes(removedFilterId)) {
+        const index = checkedFilters.value.indexOf(removedFilterId);
+        checkedFilters.value.splice(index, 1);
+    }
+}
+
+/**
+ * Function to remove all applied sub filters
+ */
+const removeAllFilters = () => {
+    checkedFilters.value = [];
 }
 </script>
